cb33c21689fa74de42a0d0df43931a952de7bc4a
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2024-05-13 02:21:13 -0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Component } from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+import './index.css'
+
+import Ass from "./assignment 3.jsx"
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Ass/>
+  </React.StrictMode>,
+)
